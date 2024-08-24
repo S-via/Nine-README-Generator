fb50ce83f1e91411c658f3c7bbeefcb71592a003
S-via <168160925+S-via@users.noreply.github.com>
2024-08-24 12:50:20 -0700

    added skeleton for javascript for prompt and write readme file

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,21 @@
 // TODO: Include packages needed for this application
+const fs = require ('fs');
+const inquire = require('inquire');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    inquire.prompt([
+        {type:'',
+            name:'',
+        message:'', },
+    ])
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile('README.md',(err) => err? console.log(err):console.log('Thank you !'))
+}
 
 // TODO: Create a function to initialize app
 function init() {}
