098359cbbc74393f20472aa73a56a62ecaa5329c
S-via <168160925+S-via@users.noreply.github.com>
2024-08-28 14:04:02 -0700

    added variable filename that containes the generatedmarkdown.js file

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const data = require("./utils/generateMarkdown.js");
+const fileName = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
 
